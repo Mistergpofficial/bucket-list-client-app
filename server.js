af76de3b453b89c420e0e6f3f598abd33c8420d7
chuky4real <ehuriahgodspower@gmail.com>
2019-03-08 12:28:04 +0100

    Initial commit to heroku

--- server.js
+++ server.js
@@ -5,24 +5,24 @@ const history = require('connect-history-api-fallback');
 
 
 const app = express();
-// app.use(serveStatic(path.join(__dirname + '/dist')));
-// app.use(history({
-//   disableDotRule: true,
-//   verbose: true
-// }));
-// app.use(express.static('static'));
-// //Serves all the request which includes /images in the url from Images folder
-// app.use('/images', express.static(__dirname + '/images'));
-// app.use('/css', express.static(__dirname + '/css'));
-// app.use('/js', express.static(__dirname + '/js'));
+app.use(serveStatic(path.join(__dirname + '/dist')));
+app.use(history({
+  disableDotRule: true,
+  verbose: true
+}));
+app.use(express.static('static'));
+//Serves all the request which includes /images in the url from Images folder
+app.use('/images', express.static(__dirname + '/images'));
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/js', express.static(__dirname + '/js'));
 
 
 
-// app.get('/*', function(req, res) {
-//     res.sendFile(path.join(__dirname + '/dist/index.html'));
-// });
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
 app.listen(port);
 
 console.log('server started '+ port);
